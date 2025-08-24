99fb0b2fde999da04749db4058663195f73c236f
luiscastro193 <luiscastro193@gmail.com>
2025-08-24 20:25:53 +0200

    Force black qr

--- qr-source.js
+++ qr-source.js
@@ -849,7 +849,7 @@ export function toSvgString(qr) {
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 ${qr.size + border * 2} ${qr.size + border * 2}" stroke="none">
 <rect width="100%" height="100%" fill="white"/>
-<path d="${parts.join(" ")}"/>
+<path d="${parts.join(" ")}" fill="black"/>
 </svg>
 `;
 }
